2a24314c046198c28584188c9c2ba127adbcf0a0
MaxGroshev <groshevmaxim84@yandex.ru>
2024-04-07 13:56:19 +0300

    minor update

--- magnetic_tape/include/utils.hpp
+++ magnetic_tape/include/utils.hpp
@@ -3,9 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <filesystem>
-#include <sys/stat.h>
 #include "time_control.hpp"
-
 
 //-----------------------------------------------------------------------------------------
 
@@ -13,30 +11,11 @@ namespace utils {
 
 namespace fs = std::filesystem;
 
-template<typename T>
-void dump_vect(const std::vector<T>& vect) {
-    for (const auto& elem : vect)
-        std::cout << elem << " ";
+template<typename It>
+void dump_cont(const It start, const It fin) {
+    for (auto iter = start; iter != fin; iter++)
+        std::cout << *iter << ' ' << std::endl;
     std::cout << '\n';
-}
-
-std::vector<std::string> get_file_name_from_dir(const char* dir_name) {
-    std::string path = std::string(fs::current_path()) + "/" + dir_name;
-    std::vector<std::string> files {};
-
-    struct stat sb;
-    for (const auto& elem_in_dir : fs::directory_iterator(path)) {
-
-        fs::path outfilename = elem_in_dir.path();
-        std::string outfilename_str = outfilename.string();
-        const char* path = outfilename_str.c_str();
-
-        if (stat(path, &sb) == 0 && !(sb.st_mode & S_IFDIR)) { //c-style(remove later)
-            files.push_back(std::string(path));
-        }
-    }
-
-    return files;
 }
 
 template<typename T>
@@ -50,8 +29,8 @@ std::vector<T> read_bin_file_into_vector(const char* path) {
     size_t f_size = fs::file_size(path);
     char* char_buf = new char[f_size];
     data_file.read(char_buf, f_size);
-    std::vector<T> data (reinterpret_cast<T*>(char_buf),
-                         reinterpret_cast<T*>(char_buf) + f_size / sizeof(T));
+    std::vector<T> data (std::bit_cast<T*>(char_buf),
+                         std::bit_cast<T*>(char_buf) + f_size / sizeof(T));
     delete [] char_buf;
     data_file.close();
     return data;
